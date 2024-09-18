6c553d4ec2b71e532e388c55e916a17b2b5e1d1d
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-09-18 18:44:11 +0200

    Update config ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  { extends: ["airbnb-base"] },
+  {
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    },
+  },
 ];
