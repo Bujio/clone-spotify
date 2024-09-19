cb4d8586c8a94544ed03f16342eb583f9532bf87
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-09-19 08:51:57 +0200

    Add middleware for not found routes and errors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => res.json({ messaje: "Hello desde server!!" }));
+app.get("/", (req, res, next) => res.json({ messaje: "Welcome to the API" }));
+
+// No route found handler
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ message: "Error. Route not found" });
+});
+
+//Error handler
+app.use((error, req, res, next) => {
+  const { statusCode = 500, message } = error;
+  res.statusCode;
+  res.json({ message });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
